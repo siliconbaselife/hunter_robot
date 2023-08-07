49fb5ce5a6774c5413b0529d39d99cecdfdfed63
旭彤 <xutong.chen@wormpex.com>
2023-08-07 21:47:55 +0800

    add

--- service/manage_service.py
+++ service/manage_service.py
@@ -11,14 +11,14 @@ def candidate_list_service(job_id, start, limit):
     chat_list = get_chats_by_job_id(job_id, start, limit)
     res_chat_list = []
     for chat in chat_list:
-        res_chat = {
+        res_chat = json.dumps({
             "candidate_id": chat[2],
             "candidate_name": chat[3],
             "source":chat[4],
             "contact":chat[6],
             "details":chat[7],
             "update_time":chat[9].strftime("%Y-%m-%d %H:%M:%S")
-        }
+        }, ensure_ascii=False)
         res_chat_list.append(res_chat)
     return res_chat_list
 
