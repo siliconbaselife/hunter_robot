992fd1049ab78359e102a33e4319389a698ce6d3
旭彤 <xutong.chen@wormpex.com>
2023-08-07 20:55:05 +0800

    add

--- dao/task_dao.py
+++ dao/task_dao.py
@@ -26,7 +26,7 @@ sql_dict = {
     "insert_sub_task_log":"insert into account_exec_log(account_id, job_id, exec_date, hello_sum_need) values ('{}','{}','{}','{}')",
     "get_sub_task_with_account_id":"select * from account_exec_log where account_id='{}' and exec_date='{}'",
     "get_job_by_id":"select * from job where job_id='{}'",
-    "get_chats_by_job_id":"select * from chat where job_id={} and contact!='NULL' order by update_time desc limit {},{}"
+    "get_chats_by_job_id":"select * from chat where job_id='{}' and contact!='NULL' order by update_time desc limit {},{}"
 }
 
 def register_job_db(job_id, platform_type, platform_id, job_name, job_jd, robot_api, job_config):
