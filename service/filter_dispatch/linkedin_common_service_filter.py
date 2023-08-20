8ed0a315a33302d330b742a6f082ae013e529a09
旭彤 <xutong.chen@wormpex.com>
2023-08-20 18:39:17 +0800

    add

---
+++ service/filter_dispatch/linkedin_common_service_filter.py
@@ -0,0 +1,51 @@
+from .utils import degree_compare
+import time
+
+def linkedin_common_service_filter(candidate_info):
+    age_range = (18, 35)
+    min_degree = '中专'
+    location = '北京'
+    job_tags = ['客服','电话销售']
+
+    age_ok = candidate_info['age'] >= age_range[0] and candidate_info['age'] <= age_range[1]
+    degree_ok = degree_compare(candidate_info['degree'], min_degree)
+    location_ok = candidate_info['exp_location']==location
+    exp_position = candidate_info['exp_position']
+    exp_salary = candidate_info['exp_salary']
+
+    if time.localtime().tm_hour > 6 and time.localtime().tm_hour < 23:
+        threshold = 300
+    else:
+        threshold = 21600
+           
+    is_active = (int(time.time()) - int(candidate_info['active_time'])) < threshold
+
+
+    has_wish = False
+    for tag in job_tags:
+        if tag in exp_position:
+            has_wish = True
+            break
+
+    has_experience = False
+    for item in candidate_info['work']:
+        if has_experience:
+            break
+        judge_str = item['position']+item['responsibility']+item['emphasis']+item.get('department', '')
+        for tag in job_tags:
+            if tag in judge_str:
+                has_experience = True
+                break
+
+    judge_result = {
+        'judge': age_ok and degree_ok and location_ok and (has_experience or has_wish) and is_active,
+        'details': {
+            'age': age_ok,
+            'degree': degree_ok,
+            'location': location_ok,
+            'experience': has_experience,
+            'wish': has_wish,
+            'is_active': is_active
+        }
+    }
+    return judge_result
---
+++ service/filter_dispatch/linkedin_preprocessor.py
@@ -0,0 +1,56 @@
+from .utils import ensure_valid
+import json
+import traceback
+from utils.log import get_logger
+from utils.config import config
+
+logger = get_logger(config['log']['log_file'])
+
+def linkedin_preprocess(raw_candidate_info):
+    try:
+        tmp = raw_candidate_info
+        cid = "" 
+        cname = tmp['title']['text'],
+        age = ""
+        degree = ""
+        active = ""
+        exp_location = ['secondarySubtitle']['text']
+        exp_salary = ""
+        position_name = ['primarySubtitle']['text']
+        education = ""
+        work = ""
+        active_time = ""
+
+        return {
+            'id': "" if cid is None else cid,
+            'name': cname,
+            'age': age,
+            'degree': degree,
+            'active': active,
+            'exp_location': exp_location,
+            'exp_salary': exp_salary,
+            'exp_position': position_name,
+            'education': education,
+            'work': work,
+            "active_time": active_time
+        } 
+
+    except BaseException as e:
+        logger.info(f'candidate filter preprocess fail  {cid}, {cname} failed for {e}, {traceback.format_exc()}')
+        with open(f'test/fail/{cid}_{cname}.json', 'w') as f:
+            f.write(json.dumps(raw_candidate_info, indent=2, ensure_ascii=False))
+        return {
+            'id': "" if cid is None else cid,
+            'name': "" if cname is None else cname,
+            'age': "" if age is None else age,
+            'degree': "" if degree is None else degree,
+            'active': "" if active is None else active,
+            'exp_location': "" if exp_location is None else exp_location,
+            'exp_salary': "" if exp_salary is None else exp_salary,
+            'exp_position': "" if position_name is None else position_name,
+            'education': "" if education is None else education,
+            'work': "" if work is None else work,
+            "active_time": "" if active_time is None else active_time
+        }
+
+    
