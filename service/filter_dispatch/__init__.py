e70c2beddb8b602aaa0bc79bfe9f1d9ea3d51a34
旭彤 <xutong.chen@wormpex.com>
2023-08-24 19:43:28 +0800

    add

--- service/filter_dispatch/__init__.py
+++ service/filter_dispatch/__init__.py
@@ -1,6 +1,9 @@
 from .boss_preprocessor import boss_preprocess
 from .linkedin_preprocessor import linkedin_preprocess
+from .maimai_preprocessor import maimai_preprocess
 from .common_custom_service_filter import common_custom_service_filter
 from .shijiazhuang_custom_service_filter import shijiazhuang_custom_service_filter
 from .shijiazhuang_qinggan_service_filter import shijiazhuang_qinggan_service_filter
 from .linkedin_common_service_filter import linkedin_common_service_filter
+from .nlp_maimai_service_filter import nlp_maimai_service_filter
+
