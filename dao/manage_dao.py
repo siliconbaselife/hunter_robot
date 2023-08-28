d29a6bcbd6e0cf9d418c440f0bf8af097a17fe49
旭彤 <xutong.chen@wormpex.com>
2023-08-28 15:14:37 +0800

    add

--- dao/manage_dao.py
+++ dao/manage_dao.py
@@ -18,7 +18,9 @@ def login_check_db(user_name):
     return dbm.query(sql_dict['login_check'].format(user_name))
 
 def jobs_query(account_id):
-    return json.loads(dbm.query(sql_dict['jobs_query'].format(account_id))[0])
+    jobs = dbm.query(sql_dict['jobs_query'].format(account_id))[0]
+    logger.info(f"test_jobs: {jobs}")
+    return json.loads(jobs)
 
 def jobs_update(jobs, account_id):
     return dbm.update(sql_dict['jobs_update'].format(jobs, account_id))
