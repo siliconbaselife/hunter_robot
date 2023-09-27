8d8ed8347dea10da6c1ff2d42029508a2b477205
旭彤 <xutong.chen@wormpex.com>
2023-09-27 13:58:38 +0800

    a

--- service/manage_service.py
+++ service/manage_service.py
@@ -47,7 +47,6 @@ def my_job_list_service(manage_account_id):
     for j_d in jobs_db:
         logger.info(f"error: {j_d[3]}")
         job_config = {} if j_d[3] == None or j_d[3] == "None" else json.loads(j_d[3])
-        robot_template =  {} if j_d[6] == None or j_d[6] == "None" else json.loads(j_d[6])
         job = {
             "job_id": j_d[0],
             "job_name": j_d[1],
@@ -55,7 +54,7 @@ def my_job_list_service(manage_account_id):
             "job_config": job_config,
             "platform_type":j_d[4],
             "robot_api":j_d[5],
-            "robot_template":robot_template
+            "robot_template":j_d[6]
         }
         ret_list.append(job)
     return ret_list
