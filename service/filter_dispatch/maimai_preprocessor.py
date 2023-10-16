efcd762c2b6a9e33f81dd0e6d850cba7b83314f7
旭彤 <xutong.chen@wormpex.com>
2023-10-16 14:44:41 +0800

    add

--- service/filter_dispatch/maimai_preprocessor.py
+++ service/filter_dispatch/maimai_preprocessor.py
@@ -6,6 +6,12 @@ from utils.config import config
 import time
 logger = get_logger(config['log']['log_file'])
 
+def process_none(s):
+    if s == None:
+        return ""
+
+
+
 def maimai_preprocess(raw_candidate_info):
     try:
         tmp = raw_candidate_info
@@ -20,7 +26,8 @@ def maimai_preprocess(raw_candidate_info):
         #文字描述感觉意义不大
         active = tmp['active_state']
         
-        cur_location = tmp['province'] + '-' + tmp['city']
+    
+        cur_location = process_none(tmp['province']) + '-' + process_none(tmp['city'])
         exp_location = {
             "region": tmp['job_preferences']['regions'],
             "cities": tmp['job_preferences']['province_cities']
@@ -29,13 +36,13 @@ def maimai_preprocess(raw_candidate_info):
         exp_salary = tmp['job_preferences']['salary']
         
         #类似运营/编辑
-        major = tmp['major']
+        major = process_none(tmp['major'])
         #类似直播电商风控
-        position_name = tmp['position']
+        position_name = process_none(tmp['position'])
         exp_positon_name = tmp['job_preferences']['positions']
         
         #工作年头
-        work_time = 0 if '年' not in tmp['work_time'] else int(tmp['work_time'].split('年')[0])
+        work_time = 0 if '年' not in process_none(tmp['work_time']) else int(process_none(tmp['work_time']).split('年')[0])
 
          #     [
             #   {
@@ -53,7 +60,7 @@ def maimai_preprocess(raw_candidate_info):
             #     "school_url": "https://i9.taou.com/maimai/p/school/small_20088e8e9e4c1f275c8e9024bbbc29fc.jpg"
             #   }
         # ]
-        education = tmp['edu']        
+        education = process_none(tmp['edu'])        
 
         
         #  "companies": [
@@ -61,7 +68,7 @@ def maimai_preprocess(raw_candidate_info):
         #     "美的集团",
         #     "同程旅行"
         #     ]
-        companies = tmp['companies']
+        companies = process_none(tmp['companies'])
         # [
         #     {
         #         "cid": 874305,
