0471188bc3c3ed1719fe7fe76dac4b68cc69f700
旭彤 <xutong.chen@wormpex.com>
2023-10-18 16:57:48 +0800

    add

--- service/chat_dispatch/base_robot.py
+++ service/chat_dispatch/base_robot.py
@@ -257,8 +257,8 @@ class BaseChatRobot(object):
                     contact['wechat'] = parse_dict['contact']
                 update_chat_contact_db(self._account_id, self._job_id, self._candidate_id, json.dumps(contact, ensure_ascii=False))
                 update_candidate_contact_db(self._candidate_id, json.dumps(contact,ensure_ascii=False))
-                candidate_name = query_candidate_by_id(self._candidate_id)[0][1]
-                send_candidate_info(self._job_id, candidate_name, contact['cv'], contact['wechat'], contact['phone'], db_history_msg)
+                # candidate_name = query_candidate_by_id(self._candidate_id)[0][1]
+                send_candidate_info(self._job_id, self._candidate_id, contact['cv'], contact['wechat'], contact['phone'], db_history_msg)
 
         if last_user_time is None:
             last_user_time = format_time(datetime.now())
