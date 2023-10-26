f79605946807a51074da15f15d393cfd49e6bd5b
旭彤 <xutong.chen@wormpex.com>
2023-10-26 20:51:04 +0800

    add

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,6 @@ import random
 import numpy as np
 import copy
 import json
-from service.task_service import get_job_by_id_service
 
 def str_is_none(str):
     return str == None or str == "" or str == "None" or str == "NULL" or  str == "NONE" or str == "Null"
