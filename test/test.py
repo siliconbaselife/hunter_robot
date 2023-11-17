fb8104f8356168e62aed5605207cd2419b933c86
李顺东 <lishundong2009@163.com>
2023-11-17 17:20:07 +0800

    增加test脚本

--- test/test.py
+++ test/test.py
@@ -1,10 +1,11 @@
 import requests
+data = {
+    'accountID': 'linkedin1',
+    'candidateID': 'candidate1',
+    'jobID': 'test1',
+    'candidateName': 'candidate test',
+    'historyMsg': [{"speaker": "robot", "msg": "hi"}, {"speaker": "user", "msg": "hi"}]
+}
 
-accountID = 'linkedin1'
-candidateID = 'candidate1'
-jobID = 'test1'
-candidateName = 'candidate test'
-historyMsg = [{"speaker": "robot", "msg": "hi"}, {"speaker": "user", "msg": "hi"}]
-
-requests.post('https://127.0.0.1:2040/recruit/candidate/chat')
+requests.post('https://127.0.0.1:2040/recruit/candidate/chat', json=data)
 
