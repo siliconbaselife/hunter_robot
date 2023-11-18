c1339af2aa23f2c5d17c85708aad936b5947e841
李顺东 <lishundong2009@163.com>
2023-11-18 11:29:12 +0800

    修改

--- service/chat_dispatch/remoly_bd_maimai_robot.py
+++ service/chat_dispatch/remoly_bd_maimai_robot.py
@@ -307,7 +307,7 @@ A.有需求 B.没有需求 C.暂时没有需求 D.无法判断
 
         logger.info(f"url: {url}")
         logger.info(f"data: {data}")
-        response = requests.post(url=url, json=data, timeout=30)
+        response = requests.post(url=url, json=data, timeout=60)
         if response.status_code != 200 or response.json()['status'] != 1:
             logger.info(
                 f"request chat algo {self._robot_api} failed, data: {data}, return {response.status_code} {response.text}")
