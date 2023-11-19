e92543cb6beb1aecbf952c775d4145b93d28fd64
旭彤 <xutong.chen@wormpex.com>
2023-11-19 11:37:44 +0800

    add

--- web/serv_main_v2.py
+++ web/serv_main_v2.py
@@ -5,7 +5,7 @@ from web.source_web import source_web
 from web.wechat_web import wechat_web
 from web.manage_web import manage_web
 from utils.config import config
-
+from dao.task_dao import *
 logger = get_logger(config['log']['log_file'])
 app = Flask("robot_backend")
 app.register_blueprint(source_web)
@@ -17,6 +17,15 @@ CORS(app, resources=r'/*')
 
 @app.route("/test")
 def test():
+    judge_result = {
+        'judge': True,
+        'details': '12312321\n213213'
+    }
+    filter_result = json.dumps(judge_result, ensure_ascii=False)
+    candidate_id = '111'
+    job_id = 'jjj'
+    prompt = 'sdfsdf'
+    insert_filter_cache(candidate_id, job_id, prompt, filter_result)
     return "Hello, World!"
 
 
