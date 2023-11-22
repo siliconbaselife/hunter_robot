fc023512060a28d64e88da3cff934c591cd2cc42
李顺东 <lishundong2009@163.com>
2023-11-22 14:00:20 +0800

    修改

--- service/chat_dispatch/remoly_bd_maimai_robot.py
+++ service/chat_dispatch/remoly_bd_maimai_robot.py
@@ -191,7 +191,7 @@ A.有需求 B.没有需求 C.暂时没有需求 D.无法判断
             if msg["speaker"] == "user":
                 continue
 
-            if "我是remoly" in msg["msg"]:
+            if "我是remoly" in msg["msg"] or "Remoly's BD" in msg["msg"]:
                 status_infos["introduction_flag"] = {"introduction_flag": True, "time": now_time}
                 return True
 
@@ -250,7 +250,7 @@ A.有需求 B.没有需求 C.暂时没有需求 D.无法判断
             m3 = "我们微信上面细聊吧"
 
         if "introduction_flag" not in flag_infos:
-            m4 = '用户打招呼(你好，hi)。回答"我是remoly的bd，我们做全球的EOR、招聘以及payroll，你们公司有相关需求吗"。'
+            m4 = '用户打招呼(你好，hi)。回答"我是remoly的BD，我们做全球的EOR、招聘以及payroll，你们公司有相关需求吗"。'
         else:
             m4 = "用户打招呼(你好，hi)。询问对方公司最近是否有新的海外人力方面的需求。"
 
