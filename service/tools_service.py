4200e7c104ee652ae677d7f4f50fe79aa1dbc6d0
旭彤 <xutong.chen@wormpex.com>
2023-12-13 21:00:54 +0800

    add

--- service/tools_service.py
+++ service/tools_service.py
@@ -45,6 +45,7 @@ def generate_resume_csv(manage_account_id, platform, start_date, end_date):
             profile_json = profile_json.replace('/', '_')
             profile_json = profile_json.replace('，', ',')
             profile_json = profile_json.replace('u0000', ',')
+            profile_json = profile_json.replace('\\', ',')
             profile = json.loads(profile_json, strict=False)
 
             # logger.info(f'download_resume_error_json, {candidate_id}, {e}, {e.args}, {traceback.format_exc()}')
