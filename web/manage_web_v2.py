ab0cd12ba99582c1d16b5dd8662e6989286e87ec
旭彤 <xutong.chen@wormpex.com>
2023-12-18 14:10:24 +0800

    add

---
+++ web/manage_web_v2.py
@@ -0,0 +1,91 @@
+from flask import Flask, Response, request
+from flask import Blueprint
+from utils.decorator import web_exception_handler
+from utils.log import get_logger
+from utils.config import config
+import json
+import math
+from utils.web_helper import get_web_res_suc_with_data, get_web_res_fail
+
+from utils.utils import encrypt, decrypt, generate_random_digits,str_is_none, get_stat_id_dict
+from utils.utils import key
+
+from service.manage_service import *
+
+
+
+manage_web_v2 = Blueprint('manage_web', __name__, template_folder='templates')
+
+logger = get_logger(config['log']['log_file'])
+
+
+@manage_web_v2.route("/backend/manage/account/register/v2", methods=['POST'])
+@web_exception_handler
+def register_account_api():
+    platform_type = request.json['platformType']
+    platform_id = request.json['platformID']
+    cookie_user_name = request.cookies.get('user_name', None)
+    if cookie_user_name == None:
+        return Response(json.dumps(get_web_res_fail("未登录"), ensure_ascii=False))
+    else:
+        manage_account_id = decrypt(cookie_user_name, key)
+    if not cookie_check_service(manage_account_id):
+        return Response(json.dumps(get_web_res_fail("用户不存在"), ensure_ascii=False))
+    # manage_account_id = request.json['manage_account_id']
+    jobs = request.json.get('jobs', [])
+    task_config = request.json.get('taskConfig', [])
+    desc = request.json.get('desc', '')
+    logger.info(f'new_account_request_v2: {manage_account_id}, {platform_type} {platform_id} {jobs} {desc} {task_config}, ')
+    account_id = f'account_{platform_type}_{platform_id}'
+
+    delete_account_by_id(account_id)
+    register_account_db_v2(account_id, platform_type, platform_id, json.dumps(jobs, ensure_ascii=False), json.dumps(task_config, ensure_ascii=False), desc, manage_account_id, 'v2')
+    logger.info(f'new_account_register_v2: {manage_account_id}, {platform_type}, {platform_id}, {jobs}, {desc},{account_id}, {task_config}')
+    ret_data = {
+        'accountID': account_id
+    }
+    return Response(json.dumps(get_web_res_suc_with_data(ret_data)))
+
+
+@manage_web_v2.route("/backend/manage/myAccountList/v2", methods=['POST'])
+@web_exception_handler
+def my_account_list_api():
+    cookie_user_name = request.cookies.get('user_name', None)
+    if cookie_user_name == None:
+        return Response(json.dumps(get_web_res_fail("未登录"), ensure_ascii=False))
+    else:
+        manage_account_id = decrypt(cookie_user_name, key)
+    if not cookie_check_service(manage_account_id):
+        return Response(json.dumps(get_web_res_fail("用户不存在"), ensure_ascii=False))
+    # manage_account_id = request.json['manage_account_id']
+    account_ret = my_account_list_service_v2(manage_account_id)
+    logger.info(f'account_list_query_result_v2:{manage_account_id}, {account_ret}')
+    return Response(json.dumps(get_web_res_suc_with_data(account_ret), ensure_ascii=False))
+
+
+@manage_web_v2.route("/backend/manage/taskUpdate/v2", methods=['POST'])
+@web_exception_handler
+def task_update_api():
+    
+    return
+
+
+@manage_web_v2.route("/backend/manage/taskActive/v2", methods=['POST'])
+@web_exception_handler
+def task_active_update_api():
+    cookie_user_name = request.cookies.get('user_name', None)
+    if cookie_user_name == None:
+        return Response(json.dumps(get_web_res_fail("未登录"), ensure_ascii=False))
+    else:
+        manage_account_id = decrypt(cookie_user_name, key)
+    if not cookie_check_service(manage_account_id):
+        return Response(json.dumps(get_web_res_fail("用户不存在"), ensure_ascii=False))
+    account_id = request.json.get('account_id', '')
+    job_id = request.json.get('job_id', '')
+    active = request.json.get('active', -1)
+    if account_id == '' or job_id == '' or active not in [0, 1]:
+        return Response(json.dumps(get_web_res_fail("参数错误"), ensure_ascii=False))
+
+    flag = update_task_active(account_id, job_id, active)
+    logger.info(f'task_active_update_api:{manage_account_id}, {account_id}, {job_id}, {active}')
+    return Response(json.dumps(get_web_res_suc_with_data(flag), ensure_ascii=False))
