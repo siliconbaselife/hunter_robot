0ecdbc39e3cc38b30accd3608c401029098d92c9
旭彤 <xutong.chen@wormpex.com>
2023-12-22 12:42:32 +0800

    add

--- service/tools_service.py
+++ service/tools_service.py
@@ -149,7 +149,7 @@ def generate_candidate_csv_by_job(job_id, start_date, end_date):
             else:
                 c_j = candidate_info[0][7].replace('\n','.')
                 c_j = c_j.replace("\'", '\"')
-                candidate_json = json.loads(c_j)
+                candidate_json = json.loads(c_j, strict=False)
                 region = candidate_info[0][4]
                 if candidate_json.get('gender', 0) == 0:
                     gender = '未知'
