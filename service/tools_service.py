ffa62c879203639306f1dca583e4c1461379f5dc
旭彤 <xutong.chen@wormpex.com>
2023-12-23 10:40:30 +0800

    add

--- service/tools_service.py
+++ service/tools_service.py
@@ -60,6 +60,11 @@ def linkedin_online_resume_upload_processor(manage_account_id, profile, platform
         if candidate_id == None or candidate_id == '':
             continue
         if len(get_resume_by_candidate_id_and_platform(candidate_id, platform, manage_account_id)) == 0 and 'profile' in p:
+            for l in p.get('profile', {}).get('languages', []):
+                language = l.get('language', '') or ''
+                l['language'] = language.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
+                des = l.get('des', '') or ''
+                l['des'] = des.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
             for e in p.get('profile', {}).get('experiences', []):
                 for w in e.get('works', []):
                     workTimeInfo = w.get('workTimeInfo', '') or ''
@@ -73,10 +78,20 @@ def linkedin_online_resume_upload_processor(manage_account_id, profile, platform
             for edu in p.get('profile', {}).get('educations', []):
                 summary = edu.get('summary', '') or ''
                 edu['summary'] = summary.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
+                degreeInfo = edu.get('degreeInfo', '') or ''
+                edu['degreeInfo'] = degreeInfo.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
+                majorInfo = edu.get('majorInfo', '') or ''
+                edu['majorInfo'] = majorInfo.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
+                timeInfo = edu.get('timeInfo', '') or ''
+                edu['timeInfo'] = timeInfo.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
+                schoolName = edu.get('schoolName', '') or ''
+                edu['schoolName'] = schoolName.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
             summary = p.get('profile', {}).get('summary', '') or ''
             role = p.get('profile', {}).get('role', '') or ''
             location = p.get('profile', {}).get('location', '') or ''
             name = p.get('profile', {}).get('name', '') or ''
+            contact_info = json.dumps(p.get('profile', {}).get('contactInfo', {}), ensure_ascii=False)
+            p['profile']['contactInfo'] = contact_info.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
             p['profile']['summary'] = summary.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
             p['profile']['role'] = role.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
             p['profile']['location'] = location.replace('"', "").replace("'", "").replace("\n", ";").replace('\"', "").replace("\'", "")
