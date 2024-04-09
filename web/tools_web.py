3826b0b22dc1b891836e7c9c0354bed1372a5b7a
db24 <db24@outlook.com>
2024-04-09 20:21:26 +0800

    f

--- web/tools_web.py
+++ web/tools_web.py
@@ -600,7 +600,7 @@ def download_profile_by_tag_web111():
         return Response(json.dumps(get_web_res_fail(error_msg), ensure_ascii=False))
     pd_data = {}
     if len(search_datas) > 0:
-        for k in search_data[0]:
+        for k in search_datas[0]:
             pd_data[k] = []
         for search_data in search_datas:
             for k in search_data:
