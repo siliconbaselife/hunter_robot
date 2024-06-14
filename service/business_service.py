547260e72bb69a7848f9eca36f549a04a7adf0bc
human <human@human.com>
2024-06-14 20:16:30 +0800

    dev db for extension

--- service/business_service.py
+++ service/business_service.py
@@ -9,6 +9,13 @@ from utils.config import config as config
 
 logger = get_logger(config['log']['business_log_file'])
 
+def history_hooks(llm_func):
+    def wrapper(*args, **kwargs):
+        res, prompt, tag = llm_func(*args, **kwargs)
+        ## save db
+        return res
+    return wrapper
+
 class BusinessConsultant:
     def __init__(self, manual_id=None):
         self._id = shortuuid.uuid() if not manual_id else manual_id
@@ -54,25 +61,28 @@ class BusinessConsultant:
         assert res_msg=='是' or res_msg=='不是', f"business internel error, _judge_jd should return 是 or 不是, but got: {res_msg}"
         return res_msg=='是'
 
+    @history_hooks
     def _continue_chat(self, question):
         assert hasattr(self, '_llm'), "business internel error, continue chat without llm object"
         res_msg = self._llm.send_message(prompt=question)
         logger.info(f'consultant [{self._id}] _continue_chat ({question}), got: {res_msg}')
-        return res_msg
+        return res_msg, question, {'msg': question}
 
+    @history_hooks
     def _first_free_chat(self, question):
         prompt = f'你是一位针对中国公司海外业务的咨询顾问，你的工作是根据你的专业知识和网络讯息解答问题。请针对以下问题做出回答\n{question}'
         res_msg = self._llm.send_message(prompt=prompt)
         logger.info(f'consultant [{self._id}] _first_free_chat ({prompt}), got: {res_msg}')
-        return res_msg
+        return res_msg, prompt, {'msg': question}
 
-
+    @history_hooks
     def _first_chat(self, src_company, target_region, job, question):
         prompt = f'你是一位针对中国公司海外业务的咨询顾问，你的工作是根据你的专业知识和网络讯息解答问题。公司 {src_company} 要在区域 {target_region} 内招聘的一个岗位 {job}，请针对以下问题做出回答\n{question}'
         res_msg = self._llm.send_message(prompt=prompt)
         logger.info(f'consultant [{self._id}] _first_chat ({question}), got: {res_msg}')
-        return res_msg
+        return res_msg, prompt, {'src_company': src_company, 'target_region': target_region, 'job': job, 'msg': question}
 
+    @history_hooks
     def _find_tgt_company(self, src_company, target_region, job, jd):
         prompt = f'你是一位针对中国公司海外业务的咨询顾问，你的工作是根据你的专业知识和网络讯息解答问题。公司 {src_company} 要在区域 {target_region} 内招聘的一个岗位 {job}，请给我合适的目标公司，需要以json的list返回目标公司中文名称的列表。以下是岗位介绍：\n{jd}'
         res_msg = self._llm.send_message(prompt=prompt)
@@ -82,8 +92,9 @@ class BusinessConsultant:
         except BaseException as e:
             logger.info(f'_find_tgt_company: parse from ({res_msg}) err: {e}, will return directly')
         logger.info(f'consultant [{self._id}] _find_tgt_company ({prompt}), got: {tgt_company_info}')
-        return tgt_company_info
+        return tgt_company_info, prompt, {'src_company': src_company, 'target_region': target_region, 'job': job, 'jd': jd}
 
+    @history_hooks
     def _platform_keyword(self, job, jd, platform='领英'):
         prompt = f'匹配这个岗位 {job}，列出搜索简历跟业务相关的英文关键词，方便我在 {platform} 做搜索，需要以json的list返回关键词的列表。以下是岗位介绍\n{jd}'
         res_msg = self._llm.send_message(prompt=prompt)
@@ -93,7 +104,7 @@ class BusinessConsultant:
         except BaseException as e:
             logger.info(f'_platform_keyword: parse from ({res_msg}) err: {e}, will return directly')
         logger.info(f'consultant [{self._id}] _platform_keyword ({prompt}), got: {keywords}')
-        return keywords
+        return keywords, prompt, {'job': job, 'jd': jd, 'platform': platform}
 
 class ConsultingFirm:
     def __init__(self):
