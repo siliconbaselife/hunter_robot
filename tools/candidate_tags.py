34e79df1d84d572c8b8f85cbad1755bd04092fba
db24 <db24@outlook.com>
2024-06-23 14:48:27 +0800

    f

--- tools/candidate_tags.py
+++ tools/candidate_tags.py
@@ -8,8 +8,12 @@ def main():
     page = 1
     limit = 10
     contact2str = True
-    data = search_profile_by_tag(manage_account_id, platform, tags, page, limit, contact2str)
-    print(data)
+    data, error_msg = search_profile_by_tag(manage_account_id, platform, tags, page, limit, contact2str)
+    if error_msg is not None:
+        print(f'{error_msg}')
+        exit(1)
+    details = data['details']
+    print(details)
 
 if __name__ == '__main__':
     main()
