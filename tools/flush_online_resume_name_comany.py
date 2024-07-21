207186640c55d24c2e16c887542f4afe387243f0
db24 <db24@outlook.com>
2024-07-21 13:55:02 +0800

    Update flush_online_resume_name_comany.py

--- tools/flush_online_resume_name_comany.py
+++ tools/flush_online_resume_name_comany.py
@@ -26,9 +26,10 @@ while True:
         company = profile['company'] if 'company' in profile else ''
         sql = f'update online_resume set name = \'{name}\', company = \'{company}\' where id = {rid}'
         print(f'{sql}')
-        exit(0)
         dbm.update(sql)
         count += 1
+        if count >= 10:
+            exit()
 
     id_start += step
     id_end += step
