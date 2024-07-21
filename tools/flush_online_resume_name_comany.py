e74fe883674186d1a3d573157701b87e9ab2d34f
db24 <db24@outlook.com>
2024-07-21 14:16:21 +0800

    Update flush_online_resume_name_comany.py

--- tools/flush_online_resume_name_comany.py
+++ tools/flush_online_resume_name_comany.py
@@ -7,31 +7,52 @@ id_start = 1
 id_end = id_start + step
 count = 0
 
-while True:
-    query_sql = 'select id, raw_profile from online_resume where id >= {} and id <= {}'.format(id_start, id_end)
+
+def main():
+    while True:
+        query_sql = 'select id, raw_profile from online_resume where id >= {} and id <= {}'.format(id_start, id_end)
+        print(query_sql)
+        rows = dbm.query(query_sql)
+        print('query {} row of {} - {}, updated = {}'.format(len(rows), id_start, id_end, count))
+        if len(rows) == 0:
+            print('done')
+            break
+
+        for row in rows:
+            rid = row[0]
+            p = row[1]
+            profile = deserialize_raw_profile(p)
+            if profile is None:
+                continue
+            if 'profile' in profile:
+                profile = profile['profile']
+            name = profile['name'] if 'name' in profile else ''
+            company = profile['company'] if 'company' in profile else ''
+            sql = f'update online_resume set name = \'{name}\', company = \'{company}\' where id = {rid}'
+            # print(f'{sql}')
+            dbm.update(sql)
+            count += 1
+
+        id_start += step
+        id_end += step
+
+
+def debug():
+
+    id_debug = 33084
+    query_sql = 'select id, raw_profile from online_resume where id >= {} and id <= {}'.format(id_debug, id_debug)
     print(query_sql)
     rows = dbm.query(query_sql)
-    print('query {} row of {} - {}, updated = {}'.format(len(rows), id_start, id_end, count))
-    if len(rows) == 0:
-        print('done')
-        break
+    row  = rows[0]
+    rid = row[0]
+    p = row[1]
+    profile = deserialize_raw_profile(p)
+    if 'profile' in profile:
+        profile = profile['profile']
+    name = profile['name'] if 'name' in profile else ''
+    company = profile['company'] if 'company' in profile else ''
+    sql = f'update online_resume set name = \'{name}\', company = \'{company}\' where id = {rid}' 
+    print(sql)
 
-    for row in rows:
-        rid = row[0]
-        p = row[1]
-        profile = deserialize_raw_profile(p)
-        if profile is None:
-            continue
-        if 'profile' in profile:
-            profile = profile['profile']
-        name = profile['name'] if 'name' in profile else ''
-        company = profile['company'] if 'company' in profile else ''
-        sql = f'update online_resume set name = \'{name}\', company = \'{company}\' where id = {rid}'
-        # print(f'{sql}')
-        dbm.update(sql)
-        count += 1
-
-    id_start += step
-    id_end += step
-
-
+if __name__ == '__main__':
+    debug()
