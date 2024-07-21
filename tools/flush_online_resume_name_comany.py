730f37c055c9f7baf3394ac255ec67301eb5d034
db24 <db24@outlook.com>
2024-07-21 13:33:23 +0800

    f

--- tools/flush_online_resume_name_comany.py
+++ tools/flush_online_resume_name_comany.py
@@ -10,10 +10,18 @@ id_end = id_start + step
 while True:
     rows = dbm.query('select id, profile from online_resume where id >= {} and id <= {}'.format(id_start, id_start))
     print('query {} row of {} - {}'.format(len(rows), id_start, id_end))
+    if len(rows) == 0:
+        print('done')
+        break
 
     for row in rows:
         rid = row[0]
         p = row[1]
-        profile = parse_profile(p, '')
+        profile = parse_profile(p)
+        name = profile['name'] if 'name' in profile else ''
+        company = profile['company'] if 'company' in profile else ''
+        dbm.update(f'update online_resume set name = \'{name}\', company = \'{company}\'')
+    id_start += step
+    id_end += step
 
 
