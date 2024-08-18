ef20787f768825833d8354cc8c7d002970db40c2
lishundong2009@163.com <lishundong2009@163.com>
2024-08-18 16:01:23 +0800

    修改

--- service/tools_service.py
+++ service/tools_service.py
@@ -177,8 +177,10 @@ def cal_work_time(experiences):
             for work in experience['works']:
                 if 'workPosition' in work and (
                         'intern' in work['workPosition'] or 'Intern' in work['workPosition']):
-                    intern = True
-                    break
+                    continue
+                if "workTimeInfo" in work:
+                    min_work_start_year = min(get_min_time_info(work['workTimeInfo'], min_work_start_year),
+                                              min_work_start_year)
         if intern:
             continue
         logger.info(f"cal_work_time {experience['timeInfo']}")
