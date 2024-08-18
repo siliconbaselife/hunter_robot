cc2bf5d31ebb311a0da01cc34d0b57e9955d1422
lishundong2009@163.com <lishundong2009@163.com>
2024-08-18 14:31:19 +0800

    增加ai parse profile的功能

--- service/tools_service.py
+++ service/tools_service.py
@@ -1800,7 +1800,7 @@ def add_tag_log(manage_account_id, platform, tag, candidate_id, flow_status, new
 
 
 def parse_profile_gpt(profile):
-    prompt_msg = f"profile[0:3500] \nThis is a LinkedIn resume of a candidate. As a headhunter, you need to analyze the resume and summarize two aspects:\n1 => Industry Experience and Expertise\n2 => Career Highlights\nThe result should be represented in JSON format with 'industry_experience' for industry experience and expertise, and 'job_info' for career highlights. \nSummarize the content in no more than 50 words, and ensure it is within 50 words.\nThe result content is returned in Chinese."
+    prompt_msg = f"{profile[0:3500]} \nThis is a LinkedIn resume of a candidate. As a headhunter, you need to analyze the resume and summarize two aspects:\n1 => Industry Experience and Expertise\n2 => Career Highlights\nThe result should be represented in JSON format with 'industry_experience' for industry experience and expertise, and 'job_info' for career highlights. \nSummarize the content in no more than 50 words, and ensure it is within 50 words.\nThe result content is returned in Chinese."
     prompt = Prompt()
     prompt.add_user_message(prompt_msg)
     output = gpt_manager.chat_task(prompt)
