412299cfe265b52d866652a85ea9eddd76daa59f
db24 <db24@outlook.com>
2024-08-18 08:27:36 +0800

    f

--- test/test_parse_profile.py
+++ test/test_parse_profile.py
@@ -5,8 +5,9 @@ platform = 'Linkedin'
 def test():
     candidate_id = 'linkedin.com/in/remichenard'
     rows = get_resume_by_candidate_ids_and_platform(manage_account_id, platform, [candidate_id], 0, 1)
-    profile = parse_profile(rows[0][1], 'need_deserialize', True)
-    print(profile)
+    r = rows[0][0]
+    profile = parse_profile(r, 'need_deserialize', True)
+    print(profile['age'])
 
 if __name__ == '__main__':
     test()
