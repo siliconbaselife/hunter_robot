935bb22c237a07cc2368e0cd16657d09e08fa771
db24 <db24@outlook.com>
2024-08-18 08:35:59 +0800

    Update test_parse_profile.py

--- test/test_parse_profile.py
+++ test/test_parse_profile.py
@@ -5,7 +5,8 @@ platform = 'Linkedin'
 def test():
     candidate_id = 'linkedin.com/in/remichenard'
     rows = get_resume_by_candidate_ids_and_platform(manage_account_id, platform, [candidate_id], 0, 1)
-    r = rows[0][0]
+    r = rows[0][1]
+    print(r)
     print(len(r))
     profile = parse_profile(r, 'need_deserialize', True)
     if profile is None:
