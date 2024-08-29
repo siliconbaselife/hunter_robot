6059a6d546e405390d0036f76d5d3b3cede52f98
lishundong2009@163.com <lishundong2009@163.com>
2024-08-29 18:50:14 +0800

    改成gpt4o

--- algo/llm_inference.py
+++ algo/llm_inference.py
@@ -46,7 +46,7 @@ class ChatGPT:
     @cost_time
     def chat(self, prompt: Prompt):
         response = openai.ChatCompletion.create(
-            model="gpt-4o-mini",
+            model="gpt-3.5-turbo",
             messages=prompt.get_messages(),
             temperature=0.2
         )
