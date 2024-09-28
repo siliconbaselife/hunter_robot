e97af50c3ca64715ba7bb2773fc16454b8b6ccb4
lishundong2009@163.com <lishundong2009@163.com>
2024-09-28 14:00:07 +0800

    修改excel导出

--- web/tools_web.py
+++ web/tools_web.py
@@ -1120,7 +1120,17 @@ def data_to_excel_file(file_path, titles, data):
 
         row_formatter = workbook.add_format()
         row_formatter.set_text_wrap(True)
-        worksheet.set_column(8, 8, 60, row_formatter)
+        worksheet.set_column(0, 0, 40, row_formatter)
+        worksheet.set_column(1, 1, 30, row_formatter)
+        worksheet.set_column(2, 2, 20, row_formatter)
+        worksheet.set_column(3, 3, 50, row_formatter)
+        worksheet.set_column(4, 4, 30, row_formatter)
+        worksheet.set_column(5, 5, 20, row_formatter)
+        worksheet.set_column(6, 6, 70, row_formatter)
+        worksheet.set_column(7, 7, 60, row_formatter)
+        worksheet.set_column(8, 8, 10, row_formatter)
+        worksheet.set_column(9, 9, 10, row_formatter)
+        worksheet.set_column(10, 10, 10, row_formatter)
         worksheet.write_row('A1', titles, title_formatter)
         count = 2
 
