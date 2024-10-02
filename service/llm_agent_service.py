eeb9e1a0f672c0b3ac721dc4e81dbdd765eb9c8b
lishundong2009@163.com <lishundong2009@163.com>
2024-10-02 15:06:25 +0800

    测试rag

--- service/llm_agent_service.py
+++ service/llm_agent_service.py
@@ -198,7 +198,7 @@ class EmbeddingAgent:
         self.db = Chroma.from_documents(documents=pages, embedding=embeddings)
 
     def cal(self, query):
-        results = self.db.get_similarity(query)
+        results = self.db.similarity_search_with_relevance_scores(query)
         for r in results:
             print(r)
 
