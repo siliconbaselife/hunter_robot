71c6029bb133b388ccf969d829e7bdcd4950427c
lishundong2009@163.com <lishundong2009@163.com>
2024-10-03 09:59:12 +0800

    增加一个llmtest

--- service/llm_agent_service.py
+++ service/llm_agent_service.py
@@ -186,7 +186,7 @@ class extractionRelationAgent:
         prompt = PromptTemplate(
             input_variables=["info", "json_format", "query"],
             template="这段文字是一个人相关的文章，请萃取出该段文字中，与问题相关的信息，并总结归纳成一个json列表，有时间或者能推算出时间。"
-                     "返回格式json如下, 如果没有相关内容txt内容为空, 返回必须是json格式, 翻译成中文, json格式如下, 一定要按照我给的这个格式返回: \n{json_format} \n"
+                     "返回格式json如下, 如果没有相关内容txt内容为空, 返回必须是能接汐的json格式，千万不要给多余的格式, 翻译成中文, json格式如下, 一定要按照我给的这个格式返回: \n{json_format} \n"
                      "文本如下: \n{info} \n 问题如下: \n{query}"
         )
         output_parser = JsonOutputParser()
