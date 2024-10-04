68c7790a25b17ae07e253ddd7ec16d42f5e44c6a
lishundong2009@163.com <lishundong2009@163.com>
2024-10-04 08:48:48 +0800

    修改

--- web/tools_web.py
+++ web/tools_web.py
@@ -1150,7 +1150,7 @@ def data_to_excel_file(file_path, titles, data):
 def download_profile_by_tag_web():
     platform = request.json.get('platform', '')
     tags = request.json.get('tags', [])
-    page = request.json.get('page', 1)
+    page = request.json.get('page', 0)
     limit = request.json.get('limit', 9999)
     if platform == '' or platform not in ('maimai', 'Boss', 'Linkedin', 'liepin') or len(
             tags) == 0 or page <= 0 or limit <= 0:
