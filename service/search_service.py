3ffd86c9a325f3a72265a2d19788cce240a57d5f
lishundong2009@163.com <lishundong2009@163.com>
2024-10-19 08:42:07 +0800

    增加搜索

--- service/search_service.py
+++ service/search_service.py
@@ -3,7 +3,7 @@ from langchain_core.tools import Tool
 from langchain_google_community import GoogleSearchAPIWrapper
 
 os.environ["GOOGLE_CSE_ID"] = "a5bb86389c8d54e04"
-os.environ["GOOGLE_API_KEY"] = "14676812439-mmcgvqqsdcof8ebts4m43l5fes6vq746.apps.googleusercontent.com"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyADsE884QVkWz_Y8X1zJMvGl3lVmJ-IbZc"
 
 
 def google_search(n, query):
