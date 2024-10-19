6d79b2f467931a772723e88eea6d5603d5059bc2
lishundong2009@163.com <lishundong2009@163.com>
2024-10-19 09:58:37 +0800

    增加搜索

--- service/llm_agent_service.py
+++ service/llm_agent_service.py
@@ -7,7 +7,8 @@ from utils.log import get_logger
 from utils.config import config
 
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser, JsonOutputParser, QuestionListOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from lanchain.retrievers.web_research import QuestionListOutputParser
 
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
