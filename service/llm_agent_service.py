f86488c639b99ec08a7c711d61ba9084902d2533
lishundong2009@163.com <lishundong2009@163.com>
2024-10-19 12:32:43 +0800

    增加搜索

--- service/llm_agent_service.py
+++ service/llm_agent_service.py
@@ -384,7 +384,7 @@ def google_search(n, query):
     #     func=search.run,
     # )
 
-    web_research_retriever = WebResearchRetriever(vectorstore=vectorstore, llm=llm, search=search)
+    web_research_retriever = WebResearchRetriever.from_llm(vectorstore=vectorstore, llm=llm, search=search)
     web_research_retriever.allow_dangerous_requests = True
     docs = web_research_retriever.get_relavant_documents(query)
 
