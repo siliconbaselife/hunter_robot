3f46ea982cfa0f7b96f9cebf02dd967b8fd45a63
lishundong2009@163.com <lishundong2009@163.com>
2024-11-02 11:41:04 +0800

    准备测试

--- test/test_search/test4.py
+++ test/test_search/test4.py
@@ -2,7 +2,7 @@ from service.business_service import agent_chat_search_service
 import time
 
 begin = time.time()
-res, _ = agent_chat_search_service("lishundong2009@163.com", "锐捷在德国对标公司")
+res, _ = agent_chat_search_service("lishundong2009@163.com", "remoly是一家做什么的公司")
 end = time.time()
 
 print(f"结果 => {res}")
