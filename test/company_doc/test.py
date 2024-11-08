d64d3782f645339952e8a15d75f2fb7d08855a1e
lishundong2009@163.com <lishundong2009@163.com>
2024-11-09 06:25:58 +0800

    修改

--- test/company_doc/test.py
+++ test/company_doc/test.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     print("company doc")
     tag = "test"
     company_id = "test.com"
-    linkedin_doc = '{"over_view": "test", "website": "test.com", "industry": "test", "company_size": "18 person", ' \
+    linkedin_doc = '{"overview": "test", "website": "test.com", "industry": "test", "company_size": "18 person", ' \
                    '"headquaters": "Chinese", "Founded": "2020", "specialties": "test"}'
     save_company_service(tag, company_id, linkedin_doc)
