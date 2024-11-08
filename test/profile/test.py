92d634b1e3f03b4772f70f34d9669453ce67c92c
lishundong2009@163.com <lishundong2009@163.com>
2024-11-09 06:22:20 +0800

    修改

--- test/profile/test.py
+++ test/profile/test.py
@@ -3,7 +3,7 @@ from service.tools_service import parse_profile_by_ai_service
 if __name__ == "__main__":
     print("parse profile")
     manage_account_id = "lishundong2009@163.com"
-    platform = "linkedin"
+    platform = "Linkedin"
     candidate_id = "linkedin.com/in/xun-golden-guo"
     use_ai = True
     language = "English"
