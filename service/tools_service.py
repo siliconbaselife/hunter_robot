d51d0c638d63ae506a7b647a7340c4f9edabd7e8
lishundong2009@163.com <lishundong2009@163.com>
2024-11-11 21:41:43 +0800

    修改

--- service/tools_service.py
+++ service/tools_service.py
@@ -2161,15 +2161,19 @@ def disable_user_tag(manage_account_id, platform, tag):
 
 
 def save_company_service(tag, company_id, linkedin_doc):
-    f = query_company_tag(tag, company_id)
-    if not f:
-        add_company_tag(tag, company_id)
+    try:
+        f = query_company_tag(tag, company_id)
+        if not f:
+            add_company_tag(tag, company_id)
 
-    linkedin_doc = linkedin_doc.replace('\n', '\\n')
-    linkedin_doc = linkedin_doc.replace('\\', '')
+        linkedin_doc = linkedin_doc.replace('\n', '\\n')
+        linkedin_doc = linkedin_doc.replace('\\', '')
 
-    f = query_company_info(company_id)
-    if not f:
-        add_company_linkedin_doc(company_id, linkedin_doc)
-    else:
-        update_company_linkedin_doc(company_id, linkedin_doc)
+        f = query_company_info(company_id)
+        if not f:
+            add_company_linkedin_doc(company_id, linkedin_doc)
+        else:
+            update_company_linkedin_doc(company_id, linkedin_doc)
+    except BaseException as e:
+        logger.error(f"save_company_service error: {e}")
+        logger.error(str(traceback.format_exc()))
