ebb87a7353fda35844b744c0e29f25a2793b5ea6
lishundong2009@163.com <lishundong2009@163.com>
2024-11-12 23:37:53 +0800

    修改

--- test/company_doc/generate_csv.py
+++ test/company_doc/generate_csv.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         company_id, linkedin_doc = row
         linkedin_doc = linkedin_doc.replace('\n', '\\n')
         linkedin_doc = linkedin_doc.replace('\\', '')
-        linkedin_doc = linkedin_doc.replace('\\"', '\\\"')
+        linkedin_doc = linkedin_doc.replace('\\"', '"')
         print(company_id)
         print(linkedin_doc)
         company_info = json.loads(linkedin_doc, strict=False)
