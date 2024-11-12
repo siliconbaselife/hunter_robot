8ed4b5f0eb7e9b0326f10eb4988bd3b4eee09231
lishundong2009@163.com <lishundong2009@163.com>
2024-11-12 23:22:04 +0800

    修改

--- test/company_doc/generate_csv.py
+++ test/company_doc/generate_csv.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
 
     for i, row in enumerate(rows):
         company_id, linkedin_doc = row
+        print(linkedin_doc)
         company_info = json.loads(linkedin_doc, strict=False)
         company_info["company_id"] = company_id
         with open(os.path.join(dir, f"{i}.json"), 'w') as f:
