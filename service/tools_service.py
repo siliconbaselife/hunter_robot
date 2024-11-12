361d01f4533b6163243330c423300b23d6ad8508
lishundong2009@163.com <lishundong2009@163.com>
2024-11-12 23:47:28 +0800

    修改

--- service/tools_service.py
+++ service/tools_service.py
@@ -2165,9 +2165,9 @@ def save_company_service(tag, company_id, linkedin_doc):
         f = query_company_tag(tag, company_id)
         if not f:
             add_company_tag(tag, company_id)
-        linkedin_doc = linkedin_doc.replace('\n', '\\n')
-        linkedin_doc = linkedin_doc.replace('\'', '\\')
-        linkedin_doc = linkedin_doc.replace('\"', '\\"')
+        linkedin_doc = linkedin_doc.replace('\n', '   ')
+        linkedin_doc = linkedin_doc.replace('\'', ' ')
+        linkedin_doc = linkedin_doc.replace('\"', ' ')
 
         f = query_company_info(company_id)
         if not f:
