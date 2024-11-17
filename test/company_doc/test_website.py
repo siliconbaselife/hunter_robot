bc789ab4f87a2cc975da9391818d4e1420977fec
lishundong2009@163.com <lishundong2009@163.com>
2024-11-17 09:49:18 +0800

    修改

--- test/company_doc/test_website.py
+++ test/company_doc/test_website.py
@@ -3,7 +3,7 @@ from langchain_community.document_loaders import WebBaseLoader
 website = "http://www.solarturbines.com"
 try:
     loader = WebBaseLoader(website)
-    loader.requests_kwargs = {'verify': False, "timeout": 5}
+    loader.requests_kwargs = {'verify': False, "timeout": 10}
     docs = loader.load()
 except BaseException as e:
     print(f"googleSearchAgent 链接 {website} 获取不到内容")
