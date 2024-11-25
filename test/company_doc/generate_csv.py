6bee26346190f74a4844914cebdf3406ec77f6e2
lishundong2009@163.com <lishundong2009@163.com>
2024-11-25 20:25:01 +0800

    修改

--- test/company_doc/generate_csv.py
+++ test/company_doc/generate_csv.py
@@ -26,7 +26,12 @@ if __name__ == "__main__":
         company_info = json.loads(linkedin_doc, strict=False)
         company_info["company_id"] = company_id
         company_info["website_doc"] = website_doc
-        company_info["google_search_doc"] = json.loads(google_search_doc, strict=False)
+
+        if google_search_doc is not None:
+            company_info["google_search_doc"] = json.loads(google_search_doc, strict=False)
+        else:
+            company_info["google_search_doc"] = None
+
         with open(os.path.join(dir, f"{i}.json"), 'w') as f:
             f.write(json.dumps(company_info))
 
