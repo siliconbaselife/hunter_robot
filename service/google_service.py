a8970526dbc317a8358233e753ec8388da7b27a6
lishundong2009@163.com <lishundong2009@163.com>
2024-12-08 12:05:54 +0800

    修改

--- service/google_service.py
+++ service/google_service.py
@@ -268,11 +268,9 @@ def send_email_content_raw(manage_account_id, platform, candidate_id, email_titl
             return
         raw_profile = rows[0][1]
 
-        logger.info(f"send_email_content_raw => send_email_content_raw rows: {raw_profile}")
-        logger.info(f"send_email_content_raw => {type(raw_profile)}")
+        # logger.info(f"send_email_content_raw => send_email_content_raw {type(raw_profile)} rows: {raw_profile}")
         raw_profile = deserialize_raw_profile(str(raw_profile))
         profile = parse_profile(raw_profile, 'no', True)
-        profile = parse_profile(profile)
 
         name = ""
         if "name" in profile:
