6764f976775046306d0d4d723e608d4bf4bb5483
lishundong2009@163.com <lishundong2009@163.com>
2025-01-19 14:20:00 +0800

    修改

--- test/test_agent/test_database_column.py
+++ test/test_agent/test_database_column.py
@@ -1,4 +1,4 @@
-from dao.private_databse_dao import query_private_tags
+from dao.private_database_dao import query_private_tags
 
 if __name__ == "__main__":
     r = query_private_tags('lishundong2009@163.com', 'huawei')
