8087629b8f58cc4fd7a27757e150a9379b851aea
lishundong2009@163.com <lishundong2009@163.com>
2025-02-15 15:05:13 +0800

    修改

--- test/test_agent/test_4o.py
+++ test/test_agent/test_4o.py
@@ -1,9 +1,9 @@
 from service.llm_agent_service import CompanyAgent
 from service.llm_agent_service import JDAgent
 
-# agent = CompanyAgent()
-# r = agent.chat('sales', '英国', '家庭储能')
+agent = CompanyAgent()
+r = agent.chat('sales', '英国', '家庭储能')
 
-jd_agent = JDAgent()
-r = jd_agent.chat('sales', '储能', '伦敦', 'lishundong1991@163.com', '')
+# jd_agent = JDAgent()
+# r = jd_agent.chat('sales', '储能', '伦敦', 'lishundong1991@163.com', '')
 print(r)
