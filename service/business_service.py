4ff3fa1f9c9189e749801607005634333d9adf85
lishundong2009@163.com <lishundong2009@163.com>
2025-02-25 01:03:18 +0800

    修改

--- service/business_service.py
+++ service/business_service.py
@@ -355,7 +355,8 @@ def agent_chat_special_service(user_id, function_key, contents):
     msg = ""
     if function_key == "benchmarking_company":
         agent = CompanyAgent()
-        msg = f"欢迎！Easy Hire AI会在这里帮助您找到与您所需职位和行业相关的公司信息。通过分析这些公司，我们将识别潜在的人才来源，并整理出可寻访的产品方向。\n可提问：\n给我目标公司名单？"
+        # msg = f"欢迎！Easy Hire AI会在这里帮助您找到与您所需职位和行业相关的公司信息。通过分析这些公司，我们将识别潜在的人才来源，并整理出可寻访的产品方向。\n可提问：\n给我目标公司名单？"
+        msg = f"目标公司"
 
     if function_key == "generate_jd":
         agent = JDAgent()
