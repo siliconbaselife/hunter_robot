d990cf810aed324ab59c0f9ac8e8d30e9e1b7736
lishundong2009@163.com <lishundong2009@163.com>
2025-04-12 08:52:40 +0800

    jd匹配测试流程

--- service/llm_agent_service.py
+++ service/llm_agent_service.py
@@ -186,7 +186,7 @@ class JDMatchAgent(object):
         chat = ChatOpenAI(model="gpt-4o-mini", temperature=0.2)
         self.prompt = PromptTemplate(
             input_variables=["jd", "profile"],
-            template="1. 接收输入的{{job_description}} 和候选人的{{candidate_resume}} \n 2. 分析职位描述和简历，评估它们之间的匹配度。 "
+            template="1. 接收输入的{jd} 和候选人的{profile} \n 2. 分析职位描述和简历，评估它们之间的匹配度。 "
                      "\n 3. 输出匹配度的评分，范围从0到100。 \n 4. 根据提供匹配的原因，解释为什么该简历与职位描述匹配或不匹配。 "
                      "\n 5. 输出结果时，不要包含任何XML标签。\n 6.返回结果需要用json格式 评分key=>score 描述key=>desc"
         )
