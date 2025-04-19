7275c5479ebe1ac119ba6fadff54ff80c202ac9c
lishundong2009@163.com <lishundong2009@163.com>
2025-04-19 10:19:34 +0800

    修改

--- service/llm_agent_service.py
+++ service/llm_agent_service.py
@@ -767,7 +767,7 @@ class CompanyServiceOrProductType:
             input_variables=["company"],
             template='帮我划分 {company} 公司的职能业务，按照 前台，中台，后台。前台按照不同事业部分类。中台分成两种，一种按照业务场景，一种按照'
                      '技术分类。后台也分成两种，一种按照业务属性，一种按照职能。'
-                     '\n返回json格式, json格式如下: \n {{"前台": [], "中台": [], "后台": []}}'
+                     '\n返回json格式, json格式如下: \n {{"前台": {{"事业部1": [], "事业部1": []}}, "中台": {{"业务场景": [], "技术": []}}, "后台": {{"业务属性": [], "职能": []}}}}'
         )
         self.chain = prompt | chat | output_parser
 
