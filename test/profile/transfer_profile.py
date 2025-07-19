501c26dfd84a554499c6f389da26e5b5361b036a
lishundong2009@163.com <lishundong2009@163.com>
2025-07-19 10:42:31 +0800

    增加导简历的东西

--- test/profile/transfer_profile.py
+++ test/profile/transfer_profile.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
             raw_profile = raw_profile.replace("\n", "\\n")
             raw_profile = raw_profile.replace("\'", "\\'")
             raw_profile = raw_profile.replace('\"', '\\"')
-            name.replace("\n", "\\n").replace("\'", "\\'").replace('\"', '\\"')
+            name = name.replace("\n", "\\n").replace("\'", "\\'").replace('\"', '\\"')
             company = company.replace("\n", "\\n").replace("\'", "\\'").replace('\"', '\\"')
 
             profiles.append({
