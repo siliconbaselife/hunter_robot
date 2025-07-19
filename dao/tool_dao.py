da742a3604b1c58c8a4ffc1bf91afc42bf981b29
lishundong2009@163.com <lishundong2009@163.com>
2025-07-19 10:38:36 +0800

    增加导简历的东西

--- dao/tool_dao.py
+++ dao/tool_dao.py
@@ -736,9 +736,11 @@ def select_all_profile_time(candidate_id):
 
 def insert_all_profile(candidate_id, platform, raw_profile, cv_url, name, company, age, race, profile_time):
     sql = f"insert into all_resume(candidate_id, platform, raw_profile, cv_url, name, company, age, race, profile_time) values('{candidate_id}', '{platform}', '{raw_profile}', '{cv_url}', '{name}', '{company}', {age}, {'null' if race is None else race}, '{profile_time}')"
+    print(sql)
     dbm.insert(sql)
 
 
 def update_all_profile(candidate_id, platform, raw_profile, cv_url, name, company, age, race, profile_time):
     sql = f"update all_resume set platform = '{platform}', raw_profile = '{raw_profile}', cv_url= '{cv_url}', name = '{name}', company = '{company}', age = '{age}', race = '{race}', profile_time = '{profile_time}' where candidate_id = '{candidate_id}'"
+    print(sql)
     dbm.update(sql)
