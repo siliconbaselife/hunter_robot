cf9811b5a607c78e8bfa4a919ba71b2758a43db2
lishundong2009@163.com <lishundong2009@163.com>
2025-07-19 10:57:13 +0800

    增加导简历的东西

--- test/profile/transfer_profile.py
+++ test/profile/transfer_profile.py
@@ -20,6 +20,9 @@ if __name__ == "__main__":
             if platform != "Linkedin":
                 continue
 
+            if raw_profile is None:
+                continue
+
             raw_profile = raw_profile.replace("\n", "\\n")
             raw_profile = raw_profile.replace("\'", "\\'")
             raw_profile = raw_profile.replace('\"', '\\"')
