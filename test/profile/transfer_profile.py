3867fa89381ce8c8df462d343681f6bbf7b99cce
lishundong2009@163.com <lishundong2009@163.com>
2025-07-19 10:30:02 +0800

    增加导简历的东西

--- test/profile/transfer_profile.py
+++ test/profile/transfer_profile.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
         for profile in profiles:
             profile_time = select_all_profile_time(profile["create_time"])
 
-            if profile_time is not None:
+            if profile_time is None:
                 insert_all_profile(profile["candidate_id"], profile["platform"], profile["raw_profile"], profile["cv_url"], profile["name"], profile["company"], profile["age"], profile["race"], profile["create_time"])
                 continue
 
