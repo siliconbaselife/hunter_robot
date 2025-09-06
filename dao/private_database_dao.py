64b48034171294ca7d71919b2679e8a96db91899
lishundong2009@163.com <lishundong2009@163.com>
2025-09-06 17:05:00 +0800

    导美国HR的简历

--- dao/private_database_dao.py
+++ dao/private_database_dao.py
@@ -12,7 +12,7 @@ table_names = {
     "Clement.hu@tbirecruit.com": {
         "美国-物流仓长-test": "warehouse",
         "美国-PhD-chinese-test": "american_phd",
-        "美国-洛杉矶-HR-test": "ameracan_hr"
+        "美国-洛杉矶-HR-test": "american_hr"
     }
 }
 
